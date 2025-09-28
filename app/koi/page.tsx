2dfccb843c41f01e6b3bc88b67a3440da04bb968
Laxenta <121664115+shelleyloosespatience@users.noreply.github.com>
2025-09-28 14:22:19 +0000

    seee

--- app/koi/page.tsx
+++ app/koi/page.tsx
@@ -213,7 +213,7 @@ const VideoBackground = ({ theme }: { theme: 'dark' | 'light' }) => {
 	const videos = [
     "/videos/Raphtalia_Girl_By_laxenta .mp4",
     "/videos/IfYouSeeThis_You_Are_cute_missKoi.webm",
-    "videos/peaceAlas.mp4",
+    "videos/MHM.mp4",
 	]
 
 	// pick one at random every render
